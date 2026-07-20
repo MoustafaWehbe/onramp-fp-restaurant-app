8b256ac75febad0ce42d9fc8b921d4f607327f3e
Hadi Chebbo <chebbohadi5@gmail.com>
2026-07-20 16:20:15 +0300

    applied token validation using zod for verify email api & added authRateLimiter to verifyEmail endpoint

--- packages/api/src/schemas/auth.schemas.ts
+++ packages/api/src/schemas/auth.schemas.ts
@@ -14,3 +14,7 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(1, "Password is required"),
 });
+
+export const verifyEmailSchema = z.object({
+  token: z.string().min(1),
+}); 
