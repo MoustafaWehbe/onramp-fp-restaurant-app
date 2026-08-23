a57ba3afab70b7f09817f2dbbd6b4861d1965d03
fatima janoun <fatimajannoun09@gmail.com>
2026-08-23 15:58:27 +0300

    redirection handeling

--- packages/web/src/routes/ProtectedRoute.tsx
+++ packages/web/src/routes/ProtectedRoute.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { LoadingSpinner } from "../components/shared/LoadingSpinner";
+import { useLocation } from "react-router-dom";
 
 export function ProtectedRoute() {
   const { user, isLoading } = useAuth();
-
+  const location = useLocation();
+  
   if (isLoading) {
     return (
       <div className="flex h-screen items-center justify-center">
@@ -13,5 +15,16 @@ export function ProtectedRoute() {
     );
   }
 
-  return user ? <Outlet /> : <Navigate to="/login" replace />;
+  if (!user) {
+    const redirect = `${location.pathname}${location.search}${location.hash}`;
+
+    return (
+      <Navigate
+        to={`/login?redirect=${encodeURIComponent(redirect)}`}
+        replace
+      />
+    );
+  }
+
+  return <Outlet />;
 }
